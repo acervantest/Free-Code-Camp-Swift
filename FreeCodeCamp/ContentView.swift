6bb230d14766bd41a6cd6959f9aa88a056eef401
Alejandro <acervantest83@gmail.com>
2025-11-07 06:21:23 -0800

    Initial Commit

---
+++ FreeCodeCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeCodeCamp
+//
+//  Created by Alejandro Cervantes on 2025-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeCodeCamp/FreeCodeCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeCodeCampApp.swift
+//  FreeCodeCamp
+//
+//  Created by Alejandro Cervantes on 2025-11-07.
+//
+
+import SwiftUI
+
+@main
+struct FreeCodeCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
