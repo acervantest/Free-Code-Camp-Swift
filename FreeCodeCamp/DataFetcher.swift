dadcc0d3153b2a027053057b0335c33a0d7d5154
Alejandro <acervantest83@gmail.com>
2025-11-17 18:07:06 -0800

    Parallel API request, HomeView refactor, buildURL method implementation

--- FreeCodeCamp/DataFetcher.swift
+++ FreeCodeCamp/DataFetcher.swift
@@ -13,17 +13,11 @@ struct DataFetcher {
     let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
     
     // https://api.themoviedb.org/3/trending/tv/day?api_key=
-    func fetchTitles(for media: String) async throws -> [Title] {
+    func fetchTitles(for media: String, by type: String) async throws -> [Title] {
         
-        guard let baseURL = tmdbBaseURL, let apiKey = tmdbAPIKey else {
-            throw NetworkError.missingConfig
-        }
+        let fetchTitlesURL = try buildURL(media: media, type: type)
         
-        guard let fetchTitlesURL = URL(string: baseURL)?
-            .appending(path: "3/trending/\(media)/day")
-            .appending(queryItems: [
-                URLQueryItem(name: "api_key", value: apiKey)
-            ]) else {
+        guard let fetchTitlesURL = fetchTitlesURL else {
             throw NetworkError.urlBuildFailed
         }
         
@@ -31,7 +25,7 @@ struct DataFetcher {
         
         guard let response = urlResponse as? HTTPURLResponse, response.statusCode == 200 else {
             throw NetworkError.badURLResponse(underlyingResponse: NSError(
-                domain: "DataFetchet",
+                domain: "DataFetcher",
                 code: (urlResponse as? HTTPURLResponse)?.statusCode ?? -1,
                 userInfo: [NSLocalizedDescriptionKey: "Invalid HTTP Response"]))
         }
@@ -41,5 +35,32 @@ struct DataFetcher {
         var titles = try decoder.decode(APIObject.self, from: data).results
         Constants.addPosterPath(to: &titles)
         return titles
+    }
+    
+    private func buildURL(media: String, type: String) throws -> URL? {
+        
+        guard let baseURL = tmdbBaseURL, let apiKey = tmdbAPIKey else {
+            throw NetworkError.missingConfig
+        }
+        
+        var path: String
+        
+        if type == "trending" {
+            path = "3/trending/\(media)/day"
+        } else if type == "top_rated" {
+            path = "3/\(media)/top_rated"
+        } else {
+            throw NetworkError.urlBuildFailed
+        }
+        
+        guard let url = URL(string: baseURL)?
+            .appending(path: path)
+            .appending(queryItems: [
+                URLQueryItem(name: "api_key", value: apiKey)
+            ]) else {
+            throw NetworkError.urlBuildFailed
+        }
+        
+        return url
     }
 }
--- FreeCodeCamp/ViewModel.swift
+++ FreeCodeCamp/ViewModel.swift
@@ -20,12 +20,24 @@ class ViewModel {
     private(set) var homeStatus: FetchStatus = .notStarted
     private let dataFetcher = DataFetcher()
     var trendingMovies: [Title] = []
+    var trendingTv: [Title] = []
+    var topRatedMovies: [Title] = []
+    var topRatedTv: [Title] = []
     
     func getTitles() async { 
         homeStatus = .fetching
         
         do {
-            trendingMovies = try await dataFetcher.fetchTitles(for: "movie")
+            async let trendingMov = dataFetcher.fetchTitles(for: "movie", by: "trending")
+            async let trendingT = dataFetcher.fetchTitles(for: "tv", by: "trending")
+            async let topRatedMov = dataFetcher.fetchTitles(for: "movie", by: "top_rated")
+            async let topRatedT = dataFetcher.fetchTitles(for: "tv", by: "top_rated")
+            
+            trendingMovies = try await trendingMov
+            trendingTv = try await trendingT
+            topRatedMovies = try await topRatedMov
+            topRatedTv = try await topRatedT
+            
             homeStatus = .success
         } catch {
             print(error)
